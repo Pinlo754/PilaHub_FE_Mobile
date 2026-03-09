8ef0db05c085203cc8e968680b1ceba57e65386d
Nguyen Thanh Phong <88322432+Pinlo754@users.noreply.github.com>
2026-03-09 09:46:29 +0700

    Feature/0903 (#13)
    * feat: fetch exercise by id
    
    * pull main
    
    ---------
    
    Co-authored-by: Mike280804 <mieunu2882004@gmail.com>

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -90,6 +90,7 @@ export type RootStackParamList = {
   FeedbackScreen: undefined;
   HealthProfiles: undefined;
 
+
   CoachDetail: { coach_id: string; selectedCoachId?: string | null };
   List: undefined;
   DailyTask: undefined;
@@ -97,8 +98,6 @@ export type RootStackParamList = {
   TraineeFeedback: undefined;
   TraineeReport: { coach_id?: string | null; exercise_id?: string | null };
   AISummary: undefined;
-  AIPractice: undefined;
-  TraineeProfile: undefined;
   AIPractice: {
     exercise_id: string;
     imgUrl: string;
@@ -174,7 +173,15 @@ const AppNavigator: React.FC = () => {
           name="TestNavigateScreen"
           component={TestNavigateScreen}
         />
+        <Stack.Screen
+          name="TestNavigateScreen"
+          component={TestNavigateScreen}
+        />
         <Stack.Screen name="CoachScreen" component={CoachScreen} />
+        <Stack.Screen
+          name="CoachRegisterSchedule"
+          component={CoachRegisterSchedule}
+        />
         <Stack.Screen
           name="CoachRegisterSchedule"
           component={CoachRegisterSchedule}
@@ -192,6 +199,14 @@ const AppNavigator: React.FC = () => {
         <Stack.Screen name="TraineeReport" component={TraineeReport} />
         <Stack.Screen name="AISummary" component={AISummary} />
         <Stack.Screen name="AIPractice" component={AIPractice} />
+        <Stack.Screen
+          name="CoachProfileScreen"
+          component={CoachProfileScreen}
+        />
+        <Stack.Screen
+          name="TraineeProfileCoachScreen"
+          component={TraineeProfileCoachScreen}
+        />
         <Stack.Screen
           name="CoachProfileScreen"
           component={CoachProfileScreen}
